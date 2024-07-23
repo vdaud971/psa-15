134a9d353c5e13bdfebf2427b007b50f555f8556
gitdemo <vhiwale61@gmail.com>
2024-07-23 15:09:09 +0530

    defect 3457 fixed the link that were not working

---
+++ git practise/git-exam/src/main/java/git_example/com/A.java
@@ -0,0 +1,7 @@
+package git_example.com;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println(100);
+    }
+}
---
+++ git practise/git-exam/src/main/java/git_example/com/GitExamApplication.java
@@ -0,0 +1,13 @@
+package git_example.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExamApplication.class, args);
+	}
+
+}
