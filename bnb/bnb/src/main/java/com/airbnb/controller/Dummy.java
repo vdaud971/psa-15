d4ecc8d461b449a4a12317b46e801fb278169bff
vdaud971 <vdaud7702@gmail.com>
2024-10-05 23:58:59 +0530

    Update Dummy.java

--- bnb/bnb/src/main/java/com/airbnb/controller/Dummy.java
+++ bnb/bnb/src/main/java/com/airbnb/controller/Dummy.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/dummy")
 public class Dummy {
 
+    // Code is been modified
     @GetMapping
     public String getMessage(){
 
